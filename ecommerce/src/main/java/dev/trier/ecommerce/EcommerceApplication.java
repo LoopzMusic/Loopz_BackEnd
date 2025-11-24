ec41c4d3492fad32a5c28b9b2322547a2aa8e37e
Victor Hugo De Pieri Justino <vdepierejustino@gmail.com>
2025-11-24 15:01:15 -0300

    Implementação das dependências do Oauth2 e configuração do Application

--- ecommerce/src/main/java/dev/trier/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/EcommerceApplication.java
@@ -3,9 +3,11 @@ package dev.trier.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebSecurity
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
