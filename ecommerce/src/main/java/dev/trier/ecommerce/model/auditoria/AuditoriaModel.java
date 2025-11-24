f95cfbcb45405cd769341f5c00104341c064a0d2
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-24 10:52:08 -0300

    Feat: Adicionado para fazer a sequencia de IDS

--- ecommerce/src/main/java/dev/trier/ecommerce/model/auditoria/AuditoriaModel.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/model/auditoria/AuditoriaModel.java
@@ -23,7 +23,7 @@ import java.time.ZoneId;
 public class AuditoriaModel {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     @RevisionNumber
     private Integer id;
 
