6e3d55622ad4d302ae0c5d79950916f95b7fe857
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-24 15:31:27 -0300

    Deleted: Apagado linhas

--- ecommerce/src/main/java/dev/trier/ecommerce/repository/ProdutoRepository.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/repository/ProdutoRepository.java
@@ -1,9 +1,13 @@
 package dev.trier.ecommerce.repository;
 
+
+import dev.trier.ecommerce.dto.produto.response.ListarProdutosResponseDto;
 import dev.trier.ecommerce.model.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,17 +18,10 @@ public interface ProdutoRepository extends JpaRepository<ProdutoModel, Integer> 
     Optional<ProdutoModel> findByCdProduto(Integer cdProduto);
     Optional<ProdutoModel> findByNmProduto(String nmProduto);
 
-//    @Query("SELECT new dev.trier.ecommerce.dto.produto.response.ProdutoIdResponseDto(" +
-//            "p.nmProduto, p.vlProduto, p.dsProduto, p.dsCategoria) " +
-//            "FROM ProdutoModel p WHERE p.dsCategoria = :categoria")
-//    List<ProdutoIdResponseDto> findAllByDsCategoria(@Param("categoria") CategoriaProduto categoria);
 
-    // Remove todos os produtos pertencentes a uma empresa (usado ao remover empresa quando não houver referência em ItemPedido)
     @Modifying
     @Transactional
     @Query("delete from ProdutoModel p where p.empresa.cdEmpresa = :cdEmpresa")
     void deleteAllByEmpresa_CdEmpresa(Integer cdEmpresa);
-
-
 
 }
