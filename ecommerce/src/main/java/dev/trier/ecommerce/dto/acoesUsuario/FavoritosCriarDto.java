2bb091089ecadbd61f82a46ffc65b1577d11e6d4
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-25 00:55:21 -0300

    Feat: Descrição de mensagem notnull

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/acoesUsuario/FavoritosCriarDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/acoesUsuario/FavoritosCriarDto.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "DTO para favoritar/desfavoritar um produto por usuário")
 public record FavoritosCriarDto(
-        @NotNull
+        @NotNull(message = "O código do usuário é obrigatório.")
         @Schema(description = "Código do usuário", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
         Integer cdUsuario,
 
-        @NotNull
+        @NotNull(message = "O código do produto é obrigatório.")
         @Schema(description = "Código do produto", example = "10", requiredMode = Schema.RequiredMode.REQUIRED)
         Integer cdProduto
 ) {}
