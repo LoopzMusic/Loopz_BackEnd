5ec3b18afa18fe2b4195dbe360795364b48ea00a
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-28 17:24:11 -0300

    Feat: Adicionado nome Cliente no retorno do login

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/auth/LoginResponse.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/auth/LoginResponse.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record LoginResponse(
         String token,
         Integer cdUsuario,
+        String nmCliente,
         String dsEmail,
         UsersRole userRole
 ) {}
