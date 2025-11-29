1b6a4e44c29b5c904c2f9ebd2eb10ef42a65b016
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-29 15:28:13 -0300

    Feat: Adicionado variável ao Dto

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/pedido/criacao/PedidoResumoTodosResponseDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/pedido/criacao/PedidoResumoTodosResponseDto.java
@@ -1,6 +1,7 @@
 package dev.trier.ecommerce.dto.pedido.criacao;
 
 import dev.trier.ecommerce.dto.pedido.ItemPedidoResponseDto;
+import dev.trier.ecommerce.model.enums.StatusPedido;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 public record PedidoResumoTodosResponseDto(
         Integer cdPedido,
         Double vlTotalPedido,
+        StatusPedido statusPedido,
         String nmCliente,
         LocalDate dtFinalizacao,
         List<ItemPedidoResponseDto> itens
