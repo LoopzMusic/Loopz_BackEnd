6acc8d52ff16d68cd39b90aa071a84183fa01b6e
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-29 03:29:30 -0300

    Feat: EndPoint de meus pedidos para admin

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/pedido/PedidoResumoResponseDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/pedido/PedidoResumoResponseDto.java
@@ -1,10 +1,12 @@
 package dev.trier.ecommerce.dto.pedido;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record PedidoResumoResponseDto(
         Integer cdPedido,
         Double valorTotal,
+        LocalDate dtFinalizacao,
         List<ItemPedidoResponseDto> itens
 ) {}
 
