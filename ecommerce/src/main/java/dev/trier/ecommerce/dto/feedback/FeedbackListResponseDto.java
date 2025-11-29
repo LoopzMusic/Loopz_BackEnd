f0b93f8e903114a7f9e2d89ac504f55221af92c3
DaviBeckha <davibeckhauser@hotmail.com>
2025-11-29 03:30:53 -0300

    Feat: Transformado em record

--- ecommerce/src/main/java/dev/trier/ecommerce/dto/feedback/FeedbackListResponseDto.java
+++ ecommerce/src/main/java/dev/trier/ecommerce/dto/feedback/FeedbackListResponseDto.java
@@ -1,20 +1,12 @@
 package dev.trier.ecommerce.dto.feedback;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class FeedbackListResponseDto {
-
-    private Integer cdFeedback;
-    private Integer nuAvaliacao;
-    private String dsComentario;
-    private String nmCliente;
-    private String nmProduto;
-    private LocalDate dtCriacao;
-}
+public record FeedbackListResponseDto(
+        Integer cdFeedback,
+        Integer nuAvaliacao,
+        String dsComentario,
+        String nmCliente,
+        String nmProduto,
+        LocalDate dtCriacao
+) {}
